392dbe76ac80451debe0da45c1183b959df22b55
Kawin Glomjai <kawin.g@taskworld.com>
2017-01-08 23:32:11 +0700

    env and firebase initilize app

--- nwb.config.js
+++ nwb.config.js
@@ -1,3 +1,26 @@
+const nib = require('nib')
+const DotEnv = require('dotenv-webpack')
 module.exports = {
-  type: 'react-app'
+  type: 'react-app',
+  webpack: {
+    loaders: {
+      stylus: { config: { use: [ nib() ] } },
+      babel: { test: /\.jsx?/ }
+    },
+    extra: {
+      resolve: { extensions: ['', '.js', '.jsx', '.json'] },
+      node: {
+        console: true,
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+      },
+      plugins: [
+        new DotEnv({ path: '.env' })
+      ]
+    },
+    define: {
+      __VERSION__: JSON.stringify(require('./package.json').version)
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ import './index.css'
 
 import React from 'react'
 import {render} from 'react-dom'
+import * as firebase from 'firebase'
 
-import App from './App'
+import App from './App.jsx'
+
+console.log(process.env)
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_HOST,
+  storageBucket: process.env.STORAGE_HOST,
+  messagingSenderId: process.env.SENDER_ID
+}
+firebase.initializeApp(config)
+const appStorage = firebase.storage()
+const appDatabase = firebase.database()
+const appAuth = firebase.auth()
 
 render(<App/>, document.querySelector('#app'))
