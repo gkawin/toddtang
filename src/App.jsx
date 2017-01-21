87ce77dd273f85c6cc3186611e2c0cd32e8c44da
Kawin Glomjai <kawin.g@taskworld.com>
2017-01-21 23:00:22 +0700

    register form

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import Layout from './layout/Layout.jsx'
 import TopNav from './top-nav/TopNav.jsx'
 
 const App = React.createClass({
+  propTypes: {
+    children: React.PropTypes.node.isRequired
+  },
+
   render () {
     return (
       <Layout
         top={(<div className='app__top-nav'><TopNav /></div>)}
         bottom={<div style={{ background: 'red' }}>FOOTER</div>}
       >
-        <div style={{ fontSize: 640 }}>MAIN CONTENT</div>
+        {this.props.children}
       </Layout>
     )
   }
