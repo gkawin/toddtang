e616fc54c4a72ceaa0ba418c8714accbb3221fe7
Kawin Glomjai <kawin.g@taskworld.com>
2017-01-29 14:00:42 +0700

    config firebase to redu store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import { render } from 'react-dom'
-import * as firebase from 'firebase'
 import { Router, Route, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 
@@ -9,18 +8,6 @@ import AppLayout from './AppLayout.jsx'
 import RegisterFormContainer from './containers/register-form/RegisterFormContainer.jsx'
 import NotFound from './not-found/NotFound.jsx'
 import configureStore from './configureStore'
-
-const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DB_HOST,
-  storageBucket: process.env.STORAGE_HOST,
-  messagingSenderId: process.env.SENDER_ID
-}
-firebase.initializeApp(config)
-// const appStorage = firebase.storage()
-// const appDatabase = firebase.database()
-// const appAuth = firebase.auth()
 
 render((
   <Provider store={configureStore()}>
