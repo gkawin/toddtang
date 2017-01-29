ebaf9e7548bff43fdc09638393e98c390ac42d70
Kawin Glomjai <kawin.g@taskworld.com>
2017-01-29 11:22:17 +0700

    link with react router

--- src/top-nav/NavigationMenu.jsx
+++ src/top-nav/NavigationMenu.jsx
@@ -1,13 +1,14 @@
 import './NavigationMenu.styl'
 
 import React from 'react'
+import { Link } from 'react-router'
 
 class NavigationMenu extends React.PureComponent {
   render () {
     return (
       <nav className='navigation-menu'>
         <ul className='navigation-menu__menu-list'>
-          <li><a href='register'>สมัครสมาชิก</a></li>
+          <li><Link href='register'>สมัครสมาชิก</Link></li>
           <li>วิธีการแทงหวย</li>
           <li>เข้าแทงหวย</li>
           <li>ติดต่อเรา</li>
