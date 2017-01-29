08cc332e2c2cb7938f781b0d257ab36d63684800
Kawin Glomjai <kawin.g@taskworld.com>
2017-01-29 11:42:42 +0700

    conpleted reselect to implemented

---
+++ src/register-form/selectors.js
@@ -0,0 +1,11 @@
+import { createSelector } from 'reselect'
+import _ from 'lodash'
+
+import * as RegisterForm from './RegisterForm'
+
+export const registerFormSelector = (state) => state.RegisterForm
+
+export const getFormStatus = createSelector(
+  registerFormSelector,
+  (registerForm) => _.get(registerForm, 'status', RegisterForm.initialState)
+)
