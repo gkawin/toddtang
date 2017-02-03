5d0e289d4077152135f5287340341cdeeb3b0b29
Kawin Glomjai <kawin.g@taskworld.com>
2017-02-03 15:54:14 +0700

    remove

--- src/register/Register.js
+++ src/register/Register.js
@@ -1,17 +1,3 @@
 import Validator from 'email-validator'
 
 export const validateEmail = (email: String) => Validator.validate(email)
-
-export const testDrop = (name) => !name
-
-export const testBlass = (name) => {
-  if (name === 'dude') return 'blah'
-  if (name === 'ka') return 'hehe'
-  if (name === 'fuck') return false
-}
-
-export const testKlass = (name) => {
-  if (name === 'zafari world') return false
-  if (name.dada === 'asdas') return 'fickcckkc'
-  return (name === 'kavin haris')
-}
