c639851dda4f67c4684ae36030b1b5bf4908530c
Kawin Glomjai <kawin.g@taskworld.com>
2017-02-03 15:09:45 +0700

    test2

--- src/register/Register.js
+++ src/register/Register.js
@@ -3,3 +3,9 @@ import Validator from 'email-validator'
 export const validateEmail = (email: String) => Validator.validate(email)
 
 export const testDrop = (name) => !name
+
+export const testBlass = (name) => {
+  if (name === 'dude') return 'blah'
+  if (name === 'ka') return 'hehe'
+  if (name === 'fuck') return false
+}
