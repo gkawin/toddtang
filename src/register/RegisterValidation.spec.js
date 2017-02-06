ae46b58abcf6e00c2cdcfa2da0af9c66f5a46186
Kawin Glomjai <kawin.g@taskworld.com>
2017-02-06 23:49:42 +0700

    add RegisterValidation test

---
+++ src/register/RegisterValidation.spec.js
@@ -0,0 +1,13 @@
+import * as RegisterValidation from './RegisterValidation'
+
+describe('RegisterValidation', () => {
+  describe('assign error validation messages', () => {
+    it('should contain `phone` `email` error messages', () => {
+
+    })
+  })
+
+  describe('test rule by rule', () => {
+    it('should be check both required and minimun allowed length', () => {})
+  })
+})
