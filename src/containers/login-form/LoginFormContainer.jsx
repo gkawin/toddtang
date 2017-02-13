bebf1b78f145e74893532501ddb52dfcc71cb739
Kawin Glomjai <kawin.g@taskworld.com>
2017-02-13 09:45:33 +0700

    login form

--- src/containers/login-form/LoginFormContainer.jsx
+++ src/containers/login-form/LoginFormContainer.jsx
@@ -1,11 +1,33 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 
 import LoginForm from '../../login/LoginForm.jsx'
 
 class LoginFormContainer extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: null,
+      password: null
+    }
+  }
+
+  handleInputChange = (e) => {
+    const value = e.target.value
+    const name = e.target.name
+    this.setState({ [name]: value })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
   render () {
     return (
-      <LoginForm />
+      <LoginForm
+        onSubmit={this.onSubmit}
+        onInputChange={this.handleInputChange}
+      />
     )
   }
 }
--- src/login/LoginForm.jsx
+++ src/login/LoginForm.jsx
@@ -1,13 +1,36 @@
+import './LoginForm.styl'
+
 import React, { PropTypes } from 'react'
 
 class LoginForm extends React.PureComponent {
   render () {
     return (
       <div className='login-form'>
-        Login Form
+        <div className='login-form__form'>
+          <form className='login-form__form-login' onSubmit={this.props.onSubmit}>
+            <input
+              type='email'
+              placeholder='email'
+              name='email'
+              onChange={this.props.onInputChange}
+            />
+            <input
+              type='password'
+              placeholder='password'
+              name='password'
+              onChange={this.props.onInputChange}
+              />
+            <button type='submit'>เข้าสู่ระบบ</button>
+          </form>
+        </div>
       </div>
     )
   }
+}
+
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired
 }
 
 export default LoginForm
