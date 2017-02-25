ef4266be47af6eaf3820975b98c0bf462df8de41
Kawin Glomjai <kawin.g@taskworld.com>
2017-02-25 16:42:52 +0700

    WTF

--- src/member-pages/MemberOverview.jsx
+++ src/member-pages/MemberOverview.jsx
@@ -1,3 +1,5 @@
+import './MemberOverview.styl'
+
 import React from 'react'
 
 import MemberMenu from './MemberMenu.jsx'
@@ -5,8 +7,8 @@ import MemberMenu from './MemberMenu.jsx'
 export default class MemberOverview extends React.Component {
   render () {
     return (
-      <div className='member-page'>
-        <div className='member-page__menu'>
+      <div className='member-overview'>
+        <div className='member-overview__menu'>
           <MemberMenu />
         </div>
       </div>
