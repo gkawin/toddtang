b3fb909ed97bf577493938e3781652e1aa6cd37a
Kawin Glomjai <kawin.g@taskworld.com>
2017-02-26 15:51:02 +0700

    member top nav roughly

--- src/member-pages/MemberTopNav.jsx
+++ src/member-pages/MemberTopNav.jsx
@@ -5,9 +5,13 @@ import React from 'react'
 export default class MemberTopNavMenu extends React.Component {
   render () {
     return (
-      <div className='member-top-nav'>
-        top nav
-      </div>
+      <section className='member-top-nav'>
+        <div className='member-top-nav__searchable'>
+          <input type='text' />
+        </div>
+        <img
+          className='member-top-nav__avatar' src='https://tw-react-prod.s3.amazonaws.com/6728f75a-ed8b-4d23-84ac-9d62ad583212.png' />
+      </section>
     )
   }
 }
