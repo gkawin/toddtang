c25174517998a2ca4b979d6545664fc9100820a5
Kawin Glomjai <kawin.g@taskworld.com>
2017-03-12 17:47:02 +0700

    add more poc functions

--- functions/index.js
+++ functions/index.js
@@ -3,3 +3,8 @@ const functions = require('firebase-functions')
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!')
 })
+
+exports.echoMessages = functions.https.onRequest((request, response) => {
+  const context = request.params.text
+  response.send(context)
+})
