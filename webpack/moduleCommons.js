c9e96ec254229b087ad0bce4100ef076b0ee9b33
Kawin Glomjai <g.kawin@live.com>
2017-11-09 18:32:18 +0700

    able to read font awesome

--- webpack/moduleCommons.js
+++ webpack/moduleCommons.js
@@ -2,7 +2,7 @@ const development = process.env.NODE_ENV === 'development'
 
 module.exports = [
   {
-    test: /\.(png|jpg|gif)$/,
+    test: /\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/,
     use: [
       {
         loader: 'file-loader',
