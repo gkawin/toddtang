abf11b5be030e9704460b1f3d3ddcdf6466a2b99
Kawin Glomjai <g.kawin@live.com>
2017-11-09 18:07:35 +0700

    fix test

--- src/register/RegisterRules.spec.js
+++ src/register/RegisterRules.spec.js
@@ -1,32 +1,31 @@
 import _ from 'lodash'
-import expect from 'expect'
 
 import * as RegisterRules from './RegisterRules'
 
 describe('Register RegisterRules', () => {
   describe('called input required', () => {
-    it.only('should return required\'s error message when input is empty', () => {
+    it('should return required\'s error message when input is empty', () => {
       const result = RegisterRules.required(null)
-      expect(result).to.be.exist()
+      expect(result).not.toBeNull()
     })
 
     it('should be nothing input is not to be an empty', () => {
       const result = RegisterRules.required('toddtang')
-      expect(result).to.be.null()
+      expect(result).toBeNull()
     })
   })
 
   describe('check email format', () => {
     it('should be nothing when fill the email format is correctly', () => {
       const result = RegisterRules.emailFormat('foo@bar.com')
-      expect(result).to.be.null()
+      expect(result).toBeNull()
     })
 
     it('should return error message if email is malformat', () => {
       const testFailureCases = [ 'bbb@.3_.com_@fsdf.cp3', '55_@dse@.com.de' ]
       _.forEach(testFailureCases, (value) => {
         const result = RegisterRules.emailFormat(value)
-        expect(result).to.be.exist()
+        expect(result).not.toBeNull()
       })
     })
   })
@@ -35,13 +34,13 @@ describe('Register RegisterRules', () => {
     it('should be nothing if input equal or more than 6 charactors', () => {
       const text = 'Nana was killed by whom?'
       const minLength = RegisterRules.minLength(6)
-      expect(minLength(text)).to.be.null()
+      expect(minLength(text)).toBeNull()
     })
 
     it('should return error message if text less than 10 charactors', () => {
       const text = 'todd'
       const minLength = RegisterRules.minLength(10)
-      expect(minLength(text)).to.be.exist()
+      expect(minLength(text)).not.toBeNull()
     })
   })
 })
