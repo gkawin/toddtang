e430a81f518611af0a513f3fac3442b43def9ca7
Kawin Glomjai <g.kawin@live.com>
2017-11-14 21:35:18 +0700

    wtf

--- client/src/app-services/authServices.js
+++ client/src/app-services/authServices.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 export const signupApplication = async (payload) => {
-  const result = await axios.post('/')
+  // const result = await axios.post('/')
 }
