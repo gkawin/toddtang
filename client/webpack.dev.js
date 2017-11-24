cdc01006129e3f829ad760c28147a5ed935a5904
Kawin Glomjai <g.kawin@live.com>
2017-11-24 14:24:22 +0700

    dev mode could not be a absolute path

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -1,15 +1,11 @@
-const path = require('path')
 const merge = require('webpack-merge')
 
 const common = require('./webpack.common.js')
-
-const BASE = path.resolve(__dirname)
 
 module.exports = merge(common, {
   devtool: 'eval',
   output: {
     filename: `[name][hash].bundle.js`,
-    path: path.join(BASE, 'asserts'),
     pathinfo: true,
     publicPath: '/'
   },
